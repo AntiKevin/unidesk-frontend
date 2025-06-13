17fc565f1efa4c39ce88987e7406ca7bf05e8aef
antikevin <kevinrodrigues@mpma.mp.br>
2025-06-13 16:35:33 -0300

    refactor: atualizando ícones e títulos de navegação no dashboard

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -2,14 +2,15 @@ import { paths } from '@/paths';
 import type { NavItemConfig } from '@/types/nav';
 
 export const navItems = [
-  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
-  { key: 'chamados', title: 'Chamados', href: paths.dashboard.customers, icon: 'users' },
+  { key: 'overview', title: 'Dashboard', href: paths.dashboard.overview, icon: 'chart-pie' },
+  { key: 'chamados', title: 'Chamados', href: paths.dashboard.chamados, icon: 'ticket' },
+  // { key: 'chamados', title: 'Chamados', href: paths.dashboard.customers, icon: 'users' },
   // { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
   { key: 'settings', title: 'Configurações', href: paths.dashboard.settings, icon: 'gear-six' },
   { key: 'account', title: 'Conta', href: paths.dashboard.account, icon: 'user' },
   // { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
   //{ key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
   //{ key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
-  { key: 'chamados2', title: 'Chamados', href: paths.dashboard.chamados, icon: 'plugs-connected' },
+  
   //{ key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
 ] satisfies NavItemConfig[];
