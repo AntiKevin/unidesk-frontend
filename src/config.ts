9e7175cac86fdbf64ab762a65440acd21f64a984
antikevin <kevin_3467@yahoo.com>
2025-07-27 01:31:08 -0300

    feat: adicionar configuração da URL base da API

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,11 @@ import { LogLevel } from '@/lib/logger';
 export interface Config {
   site: { name: string; description: string; themeColor: string; url: string };
   logLevel: keyof typeof LogLevel;
+  apiBaseUrl: string;
 }
 
 export const config: Config = {
   site: { name: 'Unidesk', description: '', themeColor: '#090a0b', url: getSiteURL() },
   logLevel: (process.env.NEXT_PUBLIC_LOG_LEVEL as keyof typeof LogLevel) ?? LogLevel.ALL,
+  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8081/api/v1',
 };
