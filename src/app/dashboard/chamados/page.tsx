e0b2869820637915fd05e5130042328da42fdf02
antikevin <kevin_3467@yahoo.com>
2025-07-28 17:25:13 -0300

    feat: passar dados de chamados para o componente ChamadosStats

--- src/app/dashboard/chamados/page.tsx
+++ src/app/dashboard/chamados/page.tsx
@@ -46,7 +46,7 @@ export default function Page(): React.JSX.Element {
                 </div>
             </Stack>
 
-            <ChamadosStats />
+            <ChamadosStats chamados={tickets} />
             <ChamadosFilters />
             <ChamadosList chamados={tickets} />
         </Stack>
--- src/components/chamados/chamados-stats.tsx
+++ src/components/chamados/chamados-stats.tsx
@@ -1,16 +1,25 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
-import { ClockCounterClockwise as ClockIcon } from '@phosphor-icons/react/dist/ssr/ClockCounterClockwise';
 import { CheckCircle as CheckIcon } from '@phosphor-icons/react/dist/ssr/CheckCircle';
+import { ClockCounterClockwise as ClockIcon } from '@phosphor-icons/react/dist/ssr/ClockCounterClockwise';
 import { Gear as GearIcon } from '@phosphor-icons/react/dist/ssr/Gear';
 import { Ticket as TicketIcon } from '@phosphor-icons/react/dist/ssr/Ticket';
+import * as React from 'react';
 
-export function ChamadosStats(): React.JSX.Element {
+export interface ChamadosStatsProps { chamados: Ticket[]; }
+export function ChamadosStats({ chamados }: ChamadosStatsProps): React.JSX.Element {
+  // Caso não receba tickets, usar array vazio
+  const data = chamados || [];
+  
+  const totalChamados = data.length;
+  const chamadosAbertos = data.filter(chamados => chamados.status.nome === 'Aberto').length;
+  const chamadosEmAndamento = data.filter(chamados => chamados.status.nome === 'Em Andamento').length;
+  const chamadosResolvidos = data.filter(chamados => chamados.status.nome === 'Resolvido').length;
+
   return (
     <Grid container spacing={3}>
       <Grid xs={12} sm={6} lg={3}>
@@ -26,7 +35,7 @@ export function ChamadosStats(): React.JSX.Element {
                 <Typography color="text.secondary" variant="overline">
                   Total de Chamados
                 </Typography>
-                <Typography variant="h5">42</Typography>
+                <Typography variant="h5">{totalChamados}</Typography>
               </Stack>
               <Box
                 sx={{
@@ -56,7 +65,7 @@ export function ChamadosStats(): React.JSX.Element {
                 <Typography color="text.secondary" variant="overline">
                   Chamados Abertos
                 </Typography>
-                <Typography variant="h5">12</Typography>
+                  <Typography variant="h5">{chamadosAbertos}</Typography>
               </Stack>
               <Box
                 sx={{
@@ -86,7 +95,7 @@ export function ChamadosStats(): React.JSX.Element {
                 <Typography color="text.secondary" variant="overline">
                   Em Andamento
                 </Typography>
-                <Typography variant="h5">8</Typography>
+                <Typography variant="h5">{chamadosEmAndamento}</Typography>
               </Stack>
               <Box
                 sx={{
@@ -116,7 +125,7 @@ export function ChamadosStats(): React.JSX.Element {
                 <Typography color="text.secondary" variant="overline">
                   Resolvidos
                 </Typography>
-                <Typography variant="h5">22</Typography>
+                <Typography variant="h5">{chamadosResolvidos}</Typography>
               </Stack>
               <Box
                 sx={{
