fa78cb85d18c7d2708fa17a38a28032419a95fa7
antikevin <kevin_3467@yahoo.com>
2025-07-28 16:23:09 -0300

    feat: refatorar método getTickets para usar mapTicketId e melhorar tipagem

--- src/services/TicketService.ts
+++ src/services/TicketService.ts
@@ -1,18 +1,28 @@
 import api from "@/lib/axios";
 
+// essa função é para mapear o ticket retornado pela API para o tipo Ticket
+// criada principalmente para garantir que o id seja tipado sem o '@id'
+const mapTicketId = (ticket: any): Ticket => {
+  const convertedTicket = {
+    id: ticket['@id'],
+    ...ticket,
+  };
+  return convertedTicket;
+};
+
 const TicketService = {
   createTicket: async (data: any) => {
     // Implementation for creating a ticket
   },
-  getTickets: async (page: number = 0, size: number = 10) => {
-    api.get<ResponsePaginated<Ticket>>(`/tickets/?page=${page}&size=${size}`)
-      .then(response => {
-        return response.data.content;
-      })
-      .catch(error => {
-        console.error("Error fetching tickets:", error);
-        throw error;
-      });
+  getTickets: async (page: number = 0, size: number = 10): Promise<Ticket[]> => {
+    try {
+      const response = await api.get<ResponsePaginated<Ticket>>(`/tickets/?page=${page}&size=${size}`);
+      const typedTickets = response.data.content.map(mapTicketId);
+      return typedTickets;
+    } catch (error) {
+      console.error("Error fetching tickets:", error);
+      throw error;
+    }
   },
   getTicketsByUser: async (userId: string) => {
     // Implementation for fetching tickets by user ID
