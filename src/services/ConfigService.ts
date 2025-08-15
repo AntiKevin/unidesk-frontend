f2c50a31d9e4eb0e951e9b8fca9c1452a63c60e5
antikevin <kevin_3467@yahoo.com>
2025-08-15 12:08:59 -0300

    refactor: comentar código do serviço de configuração

--- src/services/ConfigService.ts
+++ src/services/ConfigService.ts
@@ -1,29 +1,29 @@
-import api from "@/lib/axios";
+// import api from "@/lib/axios";
 
 
 
-const ConfigService = {
-    updatePassword: async (currentPassword: string, newPassword: string) => {
-        try {
-            const token = localStorage.getItem('auth-token'); // ou de cookie se usar SSR
+// const ConfigService = {
+//     updatePassword: async (currentPassword: string, newPassword: string) => {
+//         try {
+//             const token = localStorage.getItem('auth-token'); // ou de cookie se usar SSR
 
-            const response = await api.post(``, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${token}`, // opcional, depende da sua auth
-                },
-                body: JSON.stringify({ currentPassword, newPassword }),
-        });
+//             const response = await api.post(``, {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     Authorization: `Bearer ${token}`, // opcional, depende da sua auth
+//                 },
+//                 body: JSON.stringify({ currentPassword, newPassword }),
+//         });
 
-        if (!response.ok) {
-        const data = await response.json();
-        return { error: data.message || 'Erro ao atualizar senha' };
-        }
+//         if (!response.ok) {
+//         const data = await response.json();
+//         return { error: data.message || 'Erro ao atualizar senha' };
+//         }
 
-        return {};
-        } catch (error) {
-            console.error("Error changing password: ", error);
-        }
-    }
-}
+//         return {};
+//         } catch (error) {
+//             console.error("Error changing password: ", error);
+//         }
+//     }
+// }
